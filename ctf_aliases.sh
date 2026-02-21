1d7e328f8e8b067f0c2d0a0027564bc33134f9a3
jubeju555 <judahjosh@gmail.com>
2026-02-21 12:12:25 -0500

    update alias sheeet

--- ctf_aliases.sh
+++ ctf_aliases.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 # CTF Challenge Aliases
 # Source this file to enable quick navigation: source ~/CS466/ctf_aliases.sh
+# Or add to ~/.bashrc: source ~/CS466/ctf_aliases.sh
 
 # Base CTF directory
 export CTF_DIR="$HOME/CS466/ctf"
 
-# Alias functions for each CTF challenge
-utkctf() { cd "$CTF_DIR/01-utkctf"; }
-png_stego() { cd "$CTF_DIR/02-png_stego"; }
-minkaoctf2() { cd "$CTF_DIR/03-minkaoctf2"; }
-utklogo() { cd "$CTF_DIR/04-utklogo"; }
-stack_challenges() { cd "$CTF_DIR/05-stack_challenges"; }
-bufferoverflow() { cd "$CTF_DIR/06-bufferoverflow"; }
-bof-water18() { cd "$CTF_DIR/07-bof-water18"; }
-stackoverflow-teleport() { cd "$CTF_DIR/08-stackoverflow-teleport"; }
-bof-airport() { cd "$CTF_DIR/09-bof-airport"; }
-shellcode() { cd "$CTF_DIR/10-shellcode"; }
-shellcode2() { cd "$CTF_DIR/11-shellcode2"; }
+# Override cd command to handle CTF challenge names
+cd() {
+    case "$1" in
+        utkctf)
+            builtin cd "$CTF_DIR/01-utkctf"
+            ;;
+        png_stego)
+            builtin cd "$CTF_DIR/02-png_stego"
+            ;;
+        minkaoctf2)
+            builtin cd "$CTF_DIR/03-minkaoctf2"
+            ;;
+        utklogo)
+            builtin cd "$CTF_DIR/04-utklogo"
+            ;;
+        stack_challenges|stack)
+            builtin cd "$CTF_DIR/05-stack_challenges"
+            ;;
+        bufferoverflow|bof)
+            builtin cd "$CTF_DIR/06-bufferoverflow"
+            ;;
+        bof-water18|bof18|bofwater18)
+            builtin cd "$CTF_DIR/07-bof-water18"
+            ;;
+        stackoverflow-teleport|teleport)
+            builtin cd "$CTF_DIR/08-stackoverflow-teleport"
+            ;;
+        bof-airport)
+            builtin cd "$CTF_DIR/09-bof-airport"
+            ;;
+        shellcode|sc)
+            builtin cd "$CTF_DIR/10-shellcode"
+            ;;
+        shellcode2|sc2)
+            builtin cd "$CTF_DIR/11-shellcode2"
+            ;;
+        ctf)
+            builtin cd "$CTF_DIR"
+            ;;
+        *)
+            builtin cd "$@"
+            ;;
+    esac
+}
 
-# Shorter aliases for common ones
-alias bof18='bof-water18'
-alias bofwater18='bof-water18'
-alias bof='bufferoverflow'
-alias stack='stack_challenges'
-alias teleport='stackoverflow-teleport'
-alias shellcode='shellcode'
-alias shellcode2='shellcode2'
-alias ctf='cd $CTF_DIR'
-
-echo "CTF aliases loaded! Use: utkctf, png_stego, minkaoctf2, utklogo, stack_challenges,"
-echo "  bufferoverflow, bof-water18, stackoverflow-teleport, bof-airport, shellcode, shellcode2"
-echo "Shortcuts: bof, stack, teleport, sc, sc2, ctf"
+echo "CTF aliases loaded! Now you can use: cd bof, cd shellcode, cd stack, etc."
+echo "Available: utkctf, png_stego, minkaoctf2, utklogo, stack, bof, bof18,"
+echo "  teleport, bof-airport, shellcode (sc), shellcode2 (sc2), ctf"
